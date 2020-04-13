5bb6ccbb12d7a404ea4982fe90f7e2394bbd7c6f
TurKLoJeN <32311900+TurKLoJeN@users.noreply.github.com>
2020-04-14 02:12:50 +0300

    Online CV

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start();
+ob_start();
+$user = "turkloje_cv";
+$pass = ".(?LIQ^Q=pzf";
+
+// PDO VERİTABANI BAĞLANTISI
+
+try{
+	$db = new PDO("mysql:host=localhost; dbname=turkloje_cv; charset=utf8;",$user,$pass);
+
+}catch(PDOexception $error){
+	echo "Database Bağlantısı Kurulamadı!"; $error->getMessage();
+}
+
+
+
+
+
+ ?>

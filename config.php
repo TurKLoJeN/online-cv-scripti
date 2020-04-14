d438608f9aa56661947baaa7856f3808a1ac3118
TurKLoJeN <32311900+TurKLoJeN@users.noreply.github.com>
2020-04-14 13:12:20 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,41 @@
-<?php 
-
-session_start();
-ob_start();
-$user = "turkloje_cv";
-$pass = ".(?LIQ^Q=pzf";
-
-// PDO VERİTABANI BAĞLANTISI
-
-try{
-	$db = new PDO("mysql:host=localhost; dbname=turkloje_cv; charset=utf8;",$user,$pass);
-
-}catch(PDOexception $error){
-	echo "Database Bağlantısı Kurulamadı!"; $error->getMessage();
-}
-
-
-
-
-
+<?php 
+
+
+
+session_start();
+
+ob_start();
+
+$user = "kullanici_adi";
+
+$pass = "sifre";
+
+
+
+// PDO VERİTABANI BAĞLANTISI
+
+
+
+try{
+
+	$db = new PDO("mysql:host=localhost; dbname=veritabani_adi; charset=utf8;",$user,$pass);
+
+
+
+}catch(PDOexception $error){
+
+	echo "Database Bağlantısı Kurulamadı!"; $error->getMessage();
+
+}
+
+
+
+
+
+
+
+
+
+
+
  ?>
